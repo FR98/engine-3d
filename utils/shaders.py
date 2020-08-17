bb98efcef913249f3414a11512cc41e2cd2d3e6a
FR98 <ros18676@uvg.edu.gt>
2020-08-17 17:18:15 -0600

    Add toon shader

--- utils/shaders.py
+++ utils/shaders.py
@@ -77,3 +77,52 @@ def sombreadoCool(render, **kwargs):
         r += (texColor[2] / 255) * (1 - intensity)
 
     return r, g, b
+
+
+def toon(render, **kwargs):
+    u, v, w = kwargs['baryCoords']
+    ta, tb, tc = kwargs['texCoords']
+    na, nb, nc = kwargs['normals']
+    b, g, r = kwargs['color']
+
+    b /= 255
+    g /= 255
+    r /= 255
+
+    if render.active_texture:
+        tx = ta['x'] * u + tb['x'] * v + tc['x'] * w
+        ty = ta['y'] * u + tb['y'] * v + tc['y'] * w
+        texColor = render.active_texture.getColor(tx, ty)
+        b *= texColor[0] / 255
+        g *= texColor[1] / 255
+        r *= texColor[2] / 255
+
+    nx = na[0] * u + nb[0] * v + nc[0] * w
+    ny = na[1] * u + nb[1] * v + nc[1] * w
+    nz = na[2] * u + nb[2] * v + nc[2] * w
+
+    normal = render.vector(nx, ny, nz)
+
+    intensity = glmath.dot(normal, render.light)
+
+    if (intensity >= 0 and intensity <= 0.2):
+        intensity = 0.1
+    elif (intensity > 0.2 and intensity <= 0.4):
+        intensity = 0.3
+    elif (intensity > 0.4 and intensity <= 0.6):
+        intensity = 0.5
+    elif (intensity > 0.6 and intensity <= 0.8):
+        intensity = 0.7
+    elif (intensity > 0.8):
+        intensity = 1
+    else:
+        intensity = 0
+
+    b *= intensity
+    g *= intensity
+    r *= intensity
+
+    if intensity > 0:
+        return r, g, b
+    else:
+        return 0,0,0
